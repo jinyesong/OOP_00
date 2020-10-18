023cffc33bce5039a205138e6a12eb6b4bd2473a
SongYejin <songyejin2000@gmail.com>
2020-10-18 20:45:39 +0900

    week07

---
+++ oop_week07/oop_week07/homework_07_01.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+class AnyString {
+	std::string anyString;
+
+public:
+	AnyString(const std::string& anyString) : anyString(anyString) {}
+	std::string getAnyString() {
+		return "Stored String :: " + anyString;
+	}
+	friend std::ofstream& operator<<(std::ofstream& of, AnyString& as);
+};
+
+std::ofstream& operator<<(std::ofstream& of, AnyString& as) {
+	of << as.getAnyString();
+	return of;
+}
+
+
+int main() {
+	std::ofstream out("testOveroding.txt");
+	AnyString a("Hello, this is operator overloading test!!!");
+	std::string output = a.getAnyString();
+	out << output << std::endl;
+	out << a << std::endl;
+
+	return 0;
+}
